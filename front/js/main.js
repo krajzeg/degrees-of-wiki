b862c3490bddd0578a75642dac4fa71d95993ba5
Jakub Wasilewski <wasyl@wasyl.eu>
2016-03-03 20:17:53 +0100

    Pulled some more stuff in, namely Redux.

--- front/js/main.js
+++ front/js/main.js
@@ -1,17 +1,57 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
+import {createStore} from 'redux';
+import {Provider, connect} from 'react-redux';
 import $ from 'jquery';
-
+import _ from 'lodash';
 import 'scss/main.scss';
 
+const GREETINGS = ["Hello", "Hi", "Greetings", "A heartfelt welcome", "Salutations"];
+
+// Redux Store
+var initialState = {greeting: 0};
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'nextGreeting':
+      var newGreeting = (state.greeting + 1) % GREETINGS.length;
+      var newState = Object.assign({}, state, {greeting: newGreeting});
+      return newState;
+    default:
+      return state;
+  }
+}
+var store = createStore(reducer);
+
+// Redux Actions
+var actions = {
+  nextGreeting() { return {type: "nextGreeting"}; }
+};
+
+// React
 class Greeting extends Component {
   render() {
     return (
-      <h3>Greetings from <em>ES6</em> and <em>React</em>.</h3>
+      <div>
+        <h3>{GREETINGS[this.props.greeting]} from <em>Redux</em>.</h3>
+        <button onClick={this.props.nextGreeting}>Next!</button>
+      </div>
     );
   }
 }
 
+// React-Redux
+function mapStateToProps(state) {
+  let {greeting} = state;
+  return {greeting};
+}
+var GreetingC = connect(mapStateToProps, actions)(Greeting);
+
+// Rendering with React
 $(() => {
-  render(<Greeting/>, $('#content')[0]);
+  var main = (
+    <Provider store={store}>
+      <GreetingC/>
+    </Provider>
+  );
+  render(main, $('#content')[0])
 });
