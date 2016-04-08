b3e15008282a3f894f612f1d0b4fdc2b8e0bc865
Jakub Wasilewski <wasyl@wasyl.eu>
2016-04-08 22:33:28 +0200

    Made the Path list more readable.

--- front/js/components/Path.js
+++ front/js/components/Path.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 export default class Path extends Component {
   render() {
     const path = this.props.path;
-    const pageLinks = path.map(page =>
+    let pageLinks = path.toArray().map(page =>
       (<li><PathLink page={page} goBackTo={this.props.goBackTo}/></li>)
     );
+    pageLinks = insertSeparator(pageLinks, (<li>→</li>))
 
     return (
       <ul className="path">{pageLinks}</ul>
@@ -18,7 +19,13 @@ class PathLink extends Component {
     const page = this.props.page;
 
     return (
-      <a href="backto://{page}" onClick={() => this.props.goBackTo(page)}>{page}</a>
+      <a href="goBackTo://{page}" onClick={() => this.props.goBackTo(page)}>{page}</a>
     )
   }
+}
+
+function insertSeparator(list, separator) {
+  if (list.length <= 1) return list;
+  const butLast = _.dropRight(list, 1), last = _.last(list);
+  return _.flatten(_.map(butLast, (e) => [e, separator])).concat([last]);
 }
