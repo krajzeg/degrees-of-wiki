f492de72c83ef7f8efe72561d6f0e17dbc250497
Jakub Wasilewski <wasyl@wasyl.eu>
2016-04-18 20:55:12 +0200

    Added some basic animations to the Path component.

--- front/js/components/Path.js
+++ front/js/components/Path.js
@@ -1,36 +1,44 @@
 import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
 import {properTitle} from './helpers';
 
 export default class Path extends Component {
   render() {
     const path = this.props.path;
-    let pageLinks = path.get('pages').toArray().map(page =>
-      (<li><PathLink page={page} goBackTo={this.props.goBackTo}/></li>)
-    );
+    let pageLinks = path.get('pages').toArray().map(page => (
+      <li key={`page:${page}`}>
+        <PathLink page={page} goBackTo={this.props.goBackTo}/>
+      </li>
+    ));
     if (!this.props.won) {
-      pageLinks.push((<span>...?</span>));
+      pageLinks.push((<span key='final'>...?</span>));
     }
 
-    pageLinks = insertSeparator(pageLinks, (<li>→</li>))
+    pageLinks = insertSeparator(pageLinks, (i) => (
+      <li key={`sep:${i}`}>→</li>
+    ));
 
     return (
-      <ul className="path">{pageLinks}</ul>
-    )
+      <ReactCSSTransitionGroup component="ul" className="path"
+        transitionName='a-expand' transitionEnterTimeout={200} transitionLeaveTimeout={200}>
+        {pageLinks}
+      </ReactCSSTransitionGroup>
+    );
   }
 }
 
 class PathLink extends Component {
   render() {
     const page = this.props.page;
-
     return (
       <a className="path-link" href={`page://${page}`} onClick={() => this.props.goBackTo(page)}>{properTitle(page)}</a>
     )
   }
 }
 
-function insertSeparator(list, separator) {
+function insertSeparator(list, separatorFn) {
   if (list.length <= 1) return list;
   const butLast = _.dropRight(list, 1), last = _.last(list);
-  return _.flatten(_.map(butLast, (e) => [e, separator])).concat([last]);
+  return _.flatten(_.map(butLast, (e, index) => [e, separatorFn(index)])).concat([last]);
 }
